da54f768afa456a075871822901ee78d412f2a35
Mahesh Gadhia <maheshgadhia@Maheshs-MacBook-Air.local>
2021-10-02 21:43:56 -0400

    added s35

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Rishi Gadhia
+ */
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution35 {
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // prompt user until they enter a blank
+        // use a while loop to take input and store it into an arraylist
+        // an if condition can be used to detect if the string is blank, then terminate the loop
+
+        // use a random num generator to generate a number of the size count
+
+
+        // output the result
+    }
+}
