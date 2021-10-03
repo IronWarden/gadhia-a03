adc1f0e4dd373339bdaa83924887234fbe58a497
Mahesh Gadhia <maheshgadhia@Maheshs-MacBook-Air.local>
2021-10-02 21:49:47 -0400

    added s37

---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,30 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Rishi Gadhia
+ */
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution37 {
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // prompt the user for the minimum length, he number of special characters, and the number of numbers.
+        // Then generate a password for the user using those inputs.
+
+        generatePassword();
+    }
+
+    private static String generatePassword() {
+        // use an arraylist to store the characters
+        // arraylist<int> number, arraylist<Characters> specialCharacters, arraylist<Characters> characters
+
+        // to generate random elements of these lists we can use random.nextInt for the
+        // size of parameter specified  by the user
+
+        // do this for each arraylist and store in another arraylist
+
+        // return the password
+    }
+}
