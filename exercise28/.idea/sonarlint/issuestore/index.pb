574c1717f512d5e4ad944499ea8e5e02e2c653c4
Mahesh Gadhia <maheshgadhia@Maheshs-MacBook-Air.local>
2021-10-02 21:25:44 -0400

    added s28

---
+++ exercise28/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+M
+src/main/java/Solution28.java,c/f/cfe9e761bdbe5c5e81f96fb2b277de48dfc09040
---
+++ exercise28/src/main/java/Solution28.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Rishi Gadhia
+ */
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution28 {
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // use the try and catch exception handling method to make sure they enter integers
+
+        // we then return the sum
+        calcSum();
+
+        // output the sum 
+    }
+
+    private static void calcSum() {
+        // return the sum of all the inputs
+    }
+
+}
