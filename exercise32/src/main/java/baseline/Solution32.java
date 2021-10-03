4016aae3fe676ae1a5b72d56a1e2f1458d896943
Mahesh Gadhia <maheshgadhia@Maheshs-MacBook-Air.local>
2021-10-02 21:40:07 -0400

    added s32

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Rishi Gadhia 
+ */
+package baseline;
+import java.util.Scanner;
+
+public class Solution32 {
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // prompt and scan for the difficulty level
+        // use if conditions to continue
+
+        // throw number format exception if they enter invalid input
+        // count it as a wrong guess
+
+        // if level 1 create a random number generator between 1-10
+        // if the user input < number generated it's too low prompt and ask them to try again
+        // if input > number generated it's too high
+        // else it is even output the number of guesses
+        // a while loop is ideal for this purposes
+
+        // if level 2 create generator between 1 - 100
+
+        // if level 3 create generator between 1 - 1000
+
+    }
+}
