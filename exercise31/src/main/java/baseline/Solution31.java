c55cb581b14de0b1404ab28e03cfe3e75099ae0e
Mahesh Gadhia <maheshgadhia@Maheshs-MacBook-Air.local>
2021-10-02 21:35:47 -0400

    added s31

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Rishi Gadhia
+ */
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution31 {
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // use multiple try and catch exception Arithmetic and Number format exception
+
+        // use Karvonen formula to determine the heart rate
+        // TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+
+        // create a for loop to increment by .05  from .55 to .95
+
+        // print output with lines "-------" " | "
+
+    }
+}
