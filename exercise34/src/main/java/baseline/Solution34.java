7ebe213c107e8868210195b260b073d3460aab73
Mahesh Gadhia <maheshgadhia@Maheshs-MacBook-Air.local>
2021-10-02 21:43:05 -0400

    added s34

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Rishi Gadhia
+ */
+package baseline;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution34 {
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // create an arraylist with the employee names
+        ArrayList<String> employeeName = new ArrayList<String>();
+        employeeName.add();
+        // print out the list with each employee on a new line
+
+
+        // to remove an employee take input from the user and
+        // use remove() to remove the employee
+
+        // print the results after removal
+    }
+}
