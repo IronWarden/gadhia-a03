c072fd56c4c6b4c0e5fee31ccb769f1d9e749ee4
Mahesh Gadhia <maheshgadhia@Maheshs-MacBook-Air.local>
2021-10-02 21:41:52 -0400

    added s33

---
+++ exercise33/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+M
+src/main/java/Solution33.java,1/e/1e3bc76938f927850b701febad2591f775213708
---
+++ exercise33/src/main/java/Solution33.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Rishi Gadhia
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution33 {
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // prompt user
+        promptUser();
+        // use try and catch to make sure they enter a string
+
+        // use a rand number generator of small size
+        // use the number generated and find the correct choice 
+        // choice["Yes", "No", "Maybe", "Ask again later"]
+
+
+        // output the choice
+
+
+    }
+
+    private static void promptUser() {
+        System.out.println("What's your question?");
+    }
+}
