fd706f7eaa6ba4135d579c05b38dcaa5a9b5c79c
Mahesh Gadhia <maheshgadhia@Maheshs-MacBook-Air.local>
2021-10-02 21:51:35 -0400

    added s38

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Rishi Gadhia
+ */
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution38 {
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // prompt for the list of numbers
+        // take the input as a string then convert into an arraylist
+
+
+        filterEvenNumbers();
+
+        // output
+        
+    }
+
+    private static void filterEvenNumbers() {
+        // takes the array and return new array with the even numbers
+        // the logic do this is to iterate over the arraylist and mod the
+        // element by 2, if the mod is 0 it is even, otherwise it is odd
+        // use the arraylist function remove to remove the odd numbers
+
+
+    }
+
+}
