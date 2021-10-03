4cf699420c6fe9c1b1f813829fbfdad5bf2a00bc
Mahesh Gadhia <maheshgadhia@Maheshs-MacBook-Air.local>
2021-10-02 21:33:22 -0400

    added s30

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,20 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution30 {
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // use a nested for loop where rows < 12, and cols < 12
+
+        // multiply rows * cols
+        calcTable();
+
+        //  specify the width of the integer and align to the left
+        // using | %-d | and output the result 
+    }
+
+    private static int calcTable() {
+    }
+}
