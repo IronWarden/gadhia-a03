fd8971328e6951970cdb9f4f7e4b4613e6a28cb5
Mahesh Gadhia <maheshgadhia@Maheshs-MacBook-Air.local>
2021-10-03 21:24:56 -0400

    added s39

---
+++ exercise39/src/main/java/baseline/Employee.java
@@ -0,0 +1,18 @@
+package baseline;
+
+public class Employee extends Solution39 {
+    // create each employee with their postion, name, and separation date
+    String firstName, lastName, position, separationDate;
+
+    // use a constructor to create the employee object
+    private void employee(String firstName, String lastName, String position, String separationDate) {
+        // create the employee with these parameters
+        // create firstName
+        // create lastName
+        // create position
+        // create seperationDate
+
+    }
+
+
+}
---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Rishi Gadhia
+ */
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution39 {
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // create an employee class which creates an employee object
+
+        // map the employee
+
+    }
+}
